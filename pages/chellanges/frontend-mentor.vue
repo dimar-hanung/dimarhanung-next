4958803eebf71d745b2d21d3041179395e6918f3
dimarhanung <dimarhanung@gmail.com>
2023-04-14 00:45:55 +0700

    feat: navbar chellange frontend mentor

--- pages/chellanges/frontend-mentor.vue
+++ pages/chellanges/frontend-mentor.vue
@@ -3,10 +3,36 @@
     v-if="$route.query.preview !== 'full'"
     class="p-4 bg-slate-800 text-white flex justify-between"
   >
-    <ClientOnly>
-      <div><font-awesome-icon :icon="['fa', 'home-alt']" /> Home</div>
-    </ClientOnly>
-    <ClientOnly><font-awesome-icon :icon="['fa', 'times']" /></ClientOnly>
+    <div class="flex">
+      <ClientOnly>
+        <NuxtLink to="/"
+          ><font-awesome-icon
+            class="text-xl text-yellow-500 cursor-pointer"
+            :icon="['fa', 'home']"
+        /></NuxtLink>
+      </ClientOnly>
+      <NuxtLink
+        class="ml-8 px-3 hover:text-yellow-500"
+        to="/chellanges/frontend-mentor/qrcode"
+        ><div>1</div></NuxtLink
+      >
+
+      <NuxtLink
+        class="px-3 hover:text-yellow-500"
+        to="/chellanges/frontend-mentor/order-summary"
+        ><div>2</div></NuxtLink
+      >
+
+      <NuxtLink
+        class="px-3 hover:text-yellow-500"
+        to="/chellanges/frontend-mentor/3-single-page-developer-portofolio"
+        ><div>3</div></NuxtLink
+      >
+    </div>
+    <ClientOnly
+      ><NuxtLink :to="{ query: { preview: 'full' } }">
+        <font-awesome-icon :icon="['fa', 'times']" /> </NuxtLink
+    ></ClientOnly>
   </div>
   <NuxtPage />
 </template>
