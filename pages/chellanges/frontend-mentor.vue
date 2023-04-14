8f36d5674b707f5d31d70dd7998e2cee7a6a31d3
dimarhanung <dimarhanung@gmail.com>
2023-04-15 02:47:36 +0700

    feat: sidebar portofolio

--- pages/chellanges/frontend-mentor.vue
+++ pages/chellanges/frontend-mentor.vue
@@ -1,40 +1,36 @@
 <template>
-  <div
-    v-if="$route.query.preview !== 'full'"
-    class="p-4 bg-slate-800 text-white flex justify-between"
-  >
-    <div class="flex">
-      <ClientOnly>
-        <NuxtLink to="/"
-          ><font-awesome-icon
-            class="text-xl text-yellow-500 cursor-pointer"
-            :icon="['fa', 'home']"
-        /></NuxtLink>
-      </ClientOnly>
-      <NuxtLink
-        class="ml-8 px-3 hover:text-yellow-500"
-        to="/chellanges/frontend-mentor/qrcode"
-        ><div>1</div></NuxtLink
-      >
-
-      <NuxtLink
-        class="px-3 hover:text-yellow-500"
-        to="/chellanges/frontend-mentor/order-summary"
-        ><div>2</div></NuxtLink
-      >
-
-      <NuxtLink
-        class="px-3 hover:text-yellow-500"
-        to="/chellanges/frontend-mentor/3-single-page-developer-portofolio"
-        ><div>3</div></NuxtLink
-      >
+  <div class="flex">
+    <div
+      v-if="$route.query.preview !== 'full'"
+      class="bg-slate-800 text-white justify-between w-full max-w-[280px] hidden xl:flex"
+    >
+      <div class="w-full min-h-screen">
+        <div class="flex justify-between p-4 bg-slate-700 w-full">
+          <ClientOnly>
+            <NuxtLink to="/"
+              ><font-awesome-icon
+                class="text-xl text-yellow-500 cursor-pointer"
+                :icon="['fa', 'home']"
+            /></NuxtLink>
+            <NuxtLink :to="{ query: { preview: 'full' } }">
+              <font-awesome-icon :icon="['fa', 'times']" />
+            </NuxtLink>
+          </ClientOnly>
+        </div>
+        <div class="mt-8">
+          <NuxtLink
+            v-for="chellange in chellanges"
+            class="sidebar--link px-3 block hover:text-yellow-500 p-4"
+            :to="chellange.path"
+            ><div>{{ chellange.title }}</div></NuxtLink
+          >
+        </div>
+      </div>
     </div>
-    <ClientOnly
-      ><NuxtLink :to="{ query: { preview: 'full' } }">
-        <font-awesome-icon :icon="['fa', 'times']" /> </NuxtLink
-    ></ClientOnly>
+    <div class="w-full">
+      <NuxtPage />
+    </div>
   </div>
-  <NuxtPage />
 </template>
 
 <script setup lang="ts">
@@ -45,4 +41,76 @@ definePageMeta({
 useSeoMeta({
   title: "FrontEnd Mentor (●'◡'●)",
 });
+
+const route = useRoute();
+const router = useRouter();
+
+const chellanges = [
+  {
+    title: "1 - QR Code",
+    path: "/chellanges/frontend-mentor/qrcode",
+  },
+  {
+    title: "2 - Order Summary",
+    path: "/chellanges/frontend-mentor/order-summary",
+  },
+  {
+    title: "3 - Single Page Developer Portofolio",
+    path: "/chellanges/frontend-mentor/3-single-page-developer-portofolio",
+  },
+];
+
+// event listener when press "f" key
+const handleKeyDown = (e: KeyboardEvent) => {
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "f") {
+    const fullScreen = route.query.preview === "full";
+    router.push({
+      query: {
+        ...route.query,
+        preview: fullScreen ? undefined : "full",
+      },
+    });
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "arrowup") {
+    const index = chellanges.findIndex((chellange) => {
+      return chellange.path === route.path;
+    });
+    if (index > 0) {
+      router.push({
+        path: chellanges[index - 1].path,
+        query: { ...route.query },
+      });
+    }
+  }
+  if (e.ctrlKey && e.shiftKey && e.key.toLowerCase() === "arrowdown") {
+    const index = chellanges.findIndex((chellange) => {
+      return chellange.path === route.path;
+    });
+    if (index < chellanges.length - 1) {
+      router.push({
+        path: chellanges[index + 1].path,
+        query: { ...route.query },
+      });
+    }
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("keydown", handleKeyDown);
+  // find index of current route
+  const index = chellanges.findIndex((chellange) => {
+    return chellange.path === route.path;
+  });
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keydown", handleKeyDown);
+});
 </script>
+
+<style>
+a.sidebar--link.router-link-exact-active {
+  @apply text-yellow-400 bg-slate-700;
+}
+</style>
