ddf6109f86e8dbdf328b3ea7fc234a49a77d5417
dimarhanung <dimarhanung@gmail.com>
2023-05-08 18:17:42 +0700

    chore: upgrade nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   nitro: {
+    // preset: "vercel-edge",
     preset: "vercel-edge",
   },
 
@@ -30,42 +31,10 @@ export default defineNuxtConfig({
     "@nuxt/image-edge",
     "nuxt-simple-sitemap",
   ],
+  sitemap: {
+    enabled: true,
+  },
   tailwindcss: {
     configPath: "tailwind.config",
-  },
-  i18n: {
-    locales: ["en", "id"],
-    defaultLocale: "en",
-    vueI18n: {
-      messages: {
-        en: {
-          hello: "Hello, My Name {name}!",
-          collab: "Open to collaborate",
-
-          about: {
-            title: "About Me",
-            year: "{year} year old",
-            job: "{job} at {at}",
-            study1: "Studies for TKJ at {at}",
-            study2: "Studies for Information System at {at}",
-            address: "Lives in Tangerang Selatan,  ",
-            iLike: "I like to",
-          },
-        },
-        id: {
-          hello: "Halo ges, nama ku {name}",
-          collab: "Saya sedia join projek :D",
-          about: {
-            title: "Tentangku",
-            year: "Sudah {year} tahun di dunia",
-            job: "{job} di {at}",
-            study1: "Alumni {at} (TKJ)",
-            study2: "Sedang kuliah di {at} (Sistem Informasi)",
-            address: "Domisili di Tangerang Selatan,  ",
-            iLike: "Aku suka",
-          },
-        },
-      },
-    },
   },
 });
