d94dcee2b41a567c37f70abec272a7424b6bf9d4
dimarhanung <dimarhanung@gmail.com>
2023-05-08 18:26:06 +0700

    fix: i18n

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,35 +1,34 @@
-export default {
+export default defineI18nConfig(() => ({
+  legacy: false,
   locales: ["en", "id"],
   defaultLocale: "en",
-  vueI18n: {
-    messages: {
-      en: {
-        hello: "Hello, My Name {name}!",
-        collab: "Open to collaborate",
+  messages: {
+    en: {
+      hello: "Hello, My Name {name}!",
+      collab: "Open to collaborate",
 
-        about: {
-          title: "About Me",
-          year: "{year} year old",
-          job: "{job} at {at}",
-          study1: "Studies for TKJ at {at}",
-          study2: "Studies for Information System at {at}",
-          address: "Lives in Tangerang Selatan,  ",
-          iLike: "I like to",
-        },
+      about: {
+        title: "About Me",
+        year: "{year} year old",
+        job: "{job} at {at}",
+        study1: "Studies for TKJ at {at}",
+        study2: "Studies for Information System at {at}",
+        address: "Lives in Tangerang Selatan,  ",
+        iLike: "I like to",
       },
-      id: {
-        hello: "Halo ges, nama ku {name}",
-        collab: "Saya sedia join projek :D",
-        about: {
-          title: "Tentangku",
-          year: "Sudah {year} tahun di dunia",
-          job: "{job} di {at}",
-          study1: "Alumni {at} (TKJ)",
-          study2: "Sedang kuliah di {at} (Sistem Informasi)",
-          address: "Domisili di Tangerang Selatan,  ",
-          iLike: "Aku suka",
-        },
+    },
+    id: {
+      hello: "Halo ges, nama ku {name}",
+      collab: "Saya sedia join projek :D",
+      about: {
+        title: "Tentangku",
+        year: "Sudah {year} tahun di dunia",
+        job: "{job} di {at}",
+        study1: "Alumni {at} (TKJ)",
+        study2: "Sedang kuliah di {at} (Sistem Informasi)",
+        address: "Domisili di Tangerang Selatan,  ",
+        iLike: "Aku suka",
       },
     },
   },
-};
+}));
