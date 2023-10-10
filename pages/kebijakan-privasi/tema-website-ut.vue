9799e260a4248d7580d8ef5fc47ee68cffb31d17
Dimar Hanung P <dimarhanung@gmail.com>
2023-10-11 03:29:03 +0700

    feat: kebijakan privasi ekstensi tema website ut

---
+++ pages/kebijakan-privasi/tema-website-ut.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="grid bg-slate-200 min-h-screen place-items-center p-8">
+    <div class="container mx-auto bg-white p-8 rounded-xl shadow">
+      <h1 class="font-bold text-2xl uppercase">
+        Kebijakan Privasi untuk Tema Website UT
+      </h1>
+      <p class="font-bold">Tanggal Efektif: 11 Oktober 2023</p>
+      <p class="mt-4">
+        Terima kasih atas kepercayaan Anda dalam menginstal ekstensi "Tema
+        Website UT" ("Produk"). Kami menghargai privasi Anda dan berkomitmen
+        untuk melindunginya.
+      </p>
+      <h2 class="font-bold text-xl mt-4">1. Pengumpulan Data</h2>
+      <p>
+        Produk kami tidak mengumpulkan atau meminta data pribadi apa pun dari
+        pengguna. Kami tidak memerlukan atau meminta informasi pribadi untuk
+        menggunakan Produk kami.
+      </p>
+      <h2 class="font-bold text-xl mt-4">2. Penggunaan Data</h2>
+      <p>
+        Mengingat kami tidak mengumpulkan data pribadi, tidak ada data untuk
+        digunakan atau dibagikan dengan pihak ketiga.
+      </p>
+      <h2 class="font-bold text-xl mt-4">
+        3. Akses dan Kontrol atas Data Anda
+      </h2>
+      <p>
+        Karena kami tidak mengumpulkan data pribadi, tidak ada data yang dapat
+        diakses atau dikontrol oleh pengguna.
+      </p>
+      <h2 class="font-bold text-xl mt-4">
+        4. Perubahan pada Kebijakan Privasi
+      </h2>
+      <p>
+        Kami dapat memperbarui Kebijakan Privasi kami dari waktu ke waktu. Kami
+        akan memberitahukan Anda tentang perubahan apa pun dengan memposting
+        Kebijakan Privasi baru di halaman ini.
+      </p>
+      <h2 class="font-bold text-xl mt-4">5. Kontak Kami</h2>
+      <p>
+        Jika Anda memiliki pertanyaan tentang Kebijakan Privasi ini, silakan
+        hubungi kami melalui email di: support@temawebsiteut.com
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: "Kebijakan Privasi Ekstensi Tema Website UT",
+});
+</script>
