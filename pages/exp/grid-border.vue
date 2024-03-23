92a46444eb1d7f3d05d1579351f46e13edc4e2f3
dimar-hanung <dimarhanung@gmail.com>
2024-03-24 01:43:38 +0700

    feat(add): quotes

---
+++ pages/exp/grid-border.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="grid grid-cols-3 divide-x divide-y-4">
+    <div class="p-4">Grid Item 1</div>
+    <div class="p-4">Grid Item 2</div>
+    <div class="p-4">Grid Item 3</div>
+    <div class="p-4">Grid Item 4</div>
+    <div class="p-4">Grid Item 5</div>
+    <div class="p-4">Grid Item 6</div>
+  </div>
+</template>
+<script setup lang="ts"></script>
