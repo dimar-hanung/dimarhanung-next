6c8e15ce7a670f61934b695e0b0a2aba95a3f2a4
dimar-hanung <dimarhanung@gmail.com>
2024-03-31 04:27:28 +0700

    feat: glowing text

--- components/home/s-motive.vue
+++ components/home/s-motive.vue
@@ -4,7 +4,7 @@
     class="text-5xl text-center mt-24 h-screen grid place-items-center relative rounded-3xl"
   >
     <div>
-      <p ref="quoteContent" class="py-24 relative w-full">
+      <p ref="quoteContent" class="glow-text py-24 relative w-full">
         Sekecil apapun progress, itu adalah progress.
       </p>
       <div class="flex gap-2 justify-center place-items-center">
@@ -17,3 +17,14 @@
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped>
+.glow-text {
+  @apply text-primary-100;
+  text-shadow: 0 0 30px rgb(var(--color-primary-500)),
+    0 0 28px rgb(var(--color-primary-500)),
+    0 0 9px rgb(var(--color-primary-500)),
+    0 0 10px rgb(var(--color-primary-500)),
+    0 0 10px rgb(var(--color-primary-500));
+}
+</style>
