146ecb968f22fda1530f00ce01f10f0f10f252fa
dimar-hanung <dimarhanung@gmail.com>
2024-09-23 09:02:25 +0700

    feat(challenge): simple  gallery

---
+++ pages/challenges/random/01-gallery/components/t-item.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="border-2 overflow-hidden shadow rounded-xl w-72"
+    :class="[selceted ? 'border-blue-700' : 'border-transparent']"
+  >
+    <div>
+      <img :src="thumbUrl" alt="" />
+    </div>
+
+    <div class="p-2">
+      <h2 class="font-bold text-lg">{{ title }}</h2>
+      <hr class="my-3" />
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    thumbUrl: {
+      type: String,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+
+    selceted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {},
+};
+</script>
---
+++ pages/challenges/random/01-gallery/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    a
+
+    <div class="flex flex-wrap gap-4 justify-center">
+      <t-item
+        v-for="(item, i) in items"
+        :title="item.title"
+        :description="item.description"
+        :thumb-url="item.thumbUrl"
+        :key="`item-${i}`"
+        @click="setSelected(item)"
+        :selceted="selected.id == item.id"
+      ></t-item>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import tItem from "./components/t-item.vue";
+
+export default {
+  components: {
+    tItem,
+  },
+
+  data() {
+    return {
+      selected: {},
+      items: [
+        {
+          id: 1,
+          title: "Judul 1",
+          description: "Deskripsi Item 1",
+          thumbUrl:
+            "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/e51f5ad207d4e825a6aa705fb17a573a?_a=AQAEuiZ",
+        },
+        {
+          id: 2,
+          title: "Judul 2",
+          description: "Deskripsi Item 2",
+          thumbUrl:
+            "https://res.cloudinary.com/daily-now/image/upload/s--8p-D2SXB--/f_auto/v1724614523/ugc/content_9857f481-bd68-49c0-bac8-9c35499d35d6",
+        },
+        {
+          id: 3,
+          title: "Judul 3",
+          description: "Deskripsi Item 3",
+          thumbUrl:
+            "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/647c45926c09f434f34bc03f8e738ea8?_a=AQAEuiZ",
+        },
+      ],
+    };
+  },
+  methods: {
+    setSelected(item) {
+      this.selected = item || {};
+    },
+  },
+};
+</script>
