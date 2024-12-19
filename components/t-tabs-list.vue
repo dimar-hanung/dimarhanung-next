288e371a4b4d944cae08ed85f34d2a6bdede17c5
dimar-hanung <dimarhanung@gmail.com>
2024-12-20 01:55:42 +0700

    feat(project-detail): init

---
+++ components/t-tabs-list.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex space-x-2 mb-4 border-b border-gray-200">
+    <slot></slot>
+  </div>
+</template>
---
+++ components/t-tabs.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { provide, ref, watch } from "vue";
+
+interface Props {
+  defaultValue: string;
+  class?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  class: "",
+});
+
+const activeTab = ref(props.defaultValue);
+
+provide("activeTab", activeTab);
+
+watch(
+  () => props.defaultValue,
+  (newValue) => {
+    activeTab.value = newValue;
+  }
+);
+</script>
+
+<template>
+  <div :class="class">
+    <slot></slot>
+  </div>
+</template>
